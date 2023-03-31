8dd57bed71e3867cc1c1de941a26b679349a6a55
griff <rpuxaxa@gmail.com>
2023-03-31 21:50:18 +0200

    migrated to 2021.3.22f1

--- Assets/Death.cs
+++ Assets/Death.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
-//             other.gameObject.SendMessage("Die");
-            Application.LoadLevel(Application.loadedLevel);
+            SceneManager.LoadScene(0);
         }
     }
     
